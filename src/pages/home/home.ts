ab54051df9ac2c1a274841d0e3937de95bbf48b9
Petunia Neta <Petunianeta@gmail.com>
2018-08-03 09:09:55 -0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  filename: any = "'../../assets/imgs/dragon.jpeg'";
+  @ViewChild(Slides) slides: Slides;
   constructor(public navCtrl: NavController) {
 
   }
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
 
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
+
+  goToSlide() {
+    this.slides.slideTo(2, 500);
+  }
+  delete(){
+
+  }
 }
